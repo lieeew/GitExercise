10b1f2f840fa7980cd0eeafeec17c5add55bdd1b
leikooo <108205879+lieeew@users.noreply.github.com>
2025-08-29 15:06:50 +0800

    4th

--- src/main/java/com/leikooo/Main.java
+++ src/main/java/com/leikooo/Main.java
@@ -17,10 +17,5 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
-
-        // change
-        // change
-        // change
-        // change
     }
 }
